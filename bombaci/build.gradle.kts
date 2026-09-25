2728436878675638602222bc92bd261e29bf1728
ahmethakan-5 <ahmethakancetindogan@gmail.com>
2026-09-25 14:25:38 +0300

    Update build.gradle.kts

--- bombaci/build.gradle.kts
+++ bombaci/build.gradle.kts
@@ -11,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:81b1d424d2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+    classpath("com.android.tools.build:gradle:8.7.3")
+    classpath("com.github.recloudstream:gradle:81b1d424d2")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
